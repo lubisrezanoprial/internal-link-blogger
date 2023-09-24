4821aced6269f14ee7ba049b46065f93e8065efd
lubisrezanoprial <145378518+lubisrezanoprial@users.noreply.github.com>
2023-09-24 19:54:14 +0700

    Update internallinkwikilub.js

--- internallinkwikilub.js
+++ internallinkwikilub.js
@@ -11,20 +11,38 @@ document.addEventListener("DOMContentLoaded", function() {
   if (articleContent) {
     var keywordsWithLinks = {}; // Objek untuk melacak kata kunci yang telah diberi tautan
 
+    var textNodes = getTextNodes(articleContent);
+
     for (var keyword in internalLinks) {
       if (internalLinks.hasOwnProperty(keyword)) {
         var regex = new RegExp("\\b" + keyword + "\\b", "g");
 
-        articleContent.innerHTML = articleContent.innerHTML.replace(regex, function(match) {
+        for (var i = 0; i < textNodes.length; i++) {
+          var node = textNodes[i];
+          var text = node.nodeValue;
+
           // Cek apakah kata kunci sudah ada dalam atribut data-link
-          if (!keywordsWithLinks[keyword]) {
-            keywordsWithLinks[keyword] = true; // Tandai kata kunci sebagai sudah diberi tautan
-            return '<a href="' + internalLinks[keyword] + '" data-link="true">' + match + '</a>';
-          } else {
-            return match; // Tidak ubah yang sudah memiliki tautan
+          if (!keywordsWithLinks[keyword] && regex.test(text)) {
+            var replacedText = text.replace(regex, function(match) {
+              keywordsWithLinks[keyword] = true; // Tandai kata kunci sebagai sudah diberi tautan
+              return '<a href="' + internalLinks[keyword] + '" data-link="true">' + match + '</a>';
+            });
+
+            node.nodeValue = replacedText;
           }
-        });
+        }
       }
     }
   }
 });
+
+function getTextNodes(element) {
+  var textNodes = [];
+  var walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+
+  while (walker.nextNode()) {
+    textNodes.push(walker.currentNode);
+  }
+
+  return textNodes;
+}

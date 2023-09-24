747b1b3533477964b632744e56bbe471e4619ca9
lubisrezanoprial <145378518+lubisrezanoprial@users.noreply.github.com>
2023-09-24 17:06:48 +0700

    Update internal-link.js

--- internal-link.js
+++ internal-link.js
@@ -12,21 +12,10 @@ var internalLinks = {
 document.addEventListener("DOMContentLoaded", function() {
   var articleContent = document.getElementById("post_body");
   if (articleContent) {
-    var keywordsWithLinks = {}; // Objek untuk melacak kata kunci yang telah diberi tautan
-
     for (var keyword in internalLinks) {
       if (internalLinks.hasOwnProperty(keyword)) {
         var regex = new RegExp("\\b" + keyword + "\\b", "g");
-
-        articleContent.innerHTML = articleContent.innerHTML.replace(regex, function(match) {
-          // Cek apakah kata kunci sudah ada dalam atribut data-link
-          if (!keywordsWithLinks[keyword]) {
-            keywordsWithLinks[keyword] = true; // Tandai kata kunci sebagai sudah diberi tautan
-            return '<a href="' + internalLinks[keyword] + '" data-link="true">' + match + '</a>';
-          } else {
-            return match; // Tidak ubah yang sudah memiliki tautan
-          }
-        });
+        articleContent.innerHTML = articleContent.innerHTML.replace(regex, '<a href="' + internalLinks[keyword] + '">' + keyword + '</a>');
       }
     }
   }
